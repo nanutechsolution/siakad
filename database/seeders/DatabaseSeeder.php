3f153a01dafef5e29e75a4dce87838601365e8cf
nanu <ranusate19@gmail.com>
2026-07-14 19:30:12 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             SkalaNilaiSeeder::class,
             RefStatusVerifikasiPembayaranSeeder::class,
             BankKampusSeeder::class,
