a531acd3d79eb524dec844a6d2c4e93a5ed77fe6
nanu <ranusate19@gmail.com>
2026-07-27 13:20:28 +0800

    Update PdfCenterDashboard.php

--- app/Filament/Clusters/PdfCenter/Pages/PdfCenterDashboard.php
+++ app/Filament/Clusters/PdfCenter/Pages/PdfCenterDashboard.php
@@ -4,12 +4,13 @@ namespace App\Filament\Clusters\PdfCenter\Pages;
 
 use App\Filament\Clusters\PdfCenter\PdfCenterCluster;
 use App\Filament\Widgets\PdfCenterStatsWidget;
+use BackedEnum;
 use Filament\Pages\Page;
 
 class PdfCenterDashboard extends Page
 {
     protected string $view = 'filament.clusters.pdf-center.pages.pdf-center-dashboard';
-
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-chart-bar';
     protected static ?string $navigationLabel = 'Dashboard';
 
     protected static ?int $navigationSort = 1;
