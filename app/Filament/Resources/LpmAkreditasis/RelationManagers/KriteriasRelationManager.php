3cbe2cb8f3837fc4c07b5056ea5b09f31f0969ae
nanu <ranusate19@gmail.com>
2026-07-29 19:25:20 +0800

    Update KriteriasRelationManager.php

--- app/Filament/Resources/LpmAkreditasis/RelationManagers/KriteriasRelationManager.php
+++ app/Filament/Resources/LpmAkreditasis/RelationManagers/KriteriasRelationManager.php
@@ -4,13 +4,10 @@ namespace App\Filament\Resources\LpmAkreditasis\RelationManagers;
 
 use App\Filament\Resources\LpmAkreditasiKriterias\LpmAkreditasiKriteriaResource;
 use Filament\Actions\Action;
-use Filament\Actions\AssociateAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\DissociateAction;
-use Filament\Actions\DissociateBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
