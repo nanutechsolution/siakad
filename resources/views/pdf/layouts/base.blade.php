24942afb8d0ff7e731217ec3954c8333c9f19bf0
nanu <ranusate19@gmail.com>
2026-07-31 00:38:39 +0800

    Update base.blade.php

--- resources/views/pdf/layouts/base.blade.php
+++ resources/views/pdf/layouts/base.blade.php
@@ -186,7 +186,7 @@
     </footer>
 
     <!-- Gunakan tag main dengan margin-top agar konten tidak tertutup header fixed -->
-    <main>
+    <main style="padding-top: 50px;">
         @yield('content')
     </main>
 </body>
