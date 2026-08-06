c7af584436094f14fd6227d1a80a7306e0d72bb9
nanu <ranusate19@gmail.com>
2026-08-07 01:25:06 +0800

    update

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -14,16 +14,54 @@ class KelasSeeder extends Seeder
     {
         $prodi = RefProdi::where('kode_prodi_internal', 'TI')->firstOrFail();
 
-        $program = RefProgram::where('kode_internal', 'REG')->firstOrFail();
+        $programReguler = RefProgram::where('kode_internal', 'REG')->firstOrFail();
+        $programEkstensi = RefProgram::where('kode_internal', 'EKS')->firstOrFail();
 
-        $angkatan = RefAngkatan::where('id_tahun', 2024)->firstOrFail();
-        foreach (range('A', 'E') as $kelas) {
+        $angkatan2024 = RefAngkatan::where('id_tahun', 2024)->firstOrFail();
+        $angkatan2025 = RefAngkatan::where('id_tahun', 2025)->firstOrFail();
+
+        // ===============================
+        // REGULER 2024
+        // ===============================
+        foreach (['A', 'B', 'C', 'D', 'E'] as $kelas) {
             Kelas::updateOrCreate(
                 [
-                    'nama_kelas' => $kelas,
-                    'prodi_id' => $prodi->id,
-                    'program_id' => $program->id,
-                    'angkatan_id' => $angkatan->id_tahun,
+                    'nama_kelas'  => $kelas,
+                    'prodi_id'    => $prodi->id,
+                    'program_id'  => $programReguler->id,
+                    'angkatan_id' => $angkatan2024->id,
+                ],
+                [
+                    'kapasitas' => 40,
+                ]
+            );
+        }
+
+        // ===============================
+        // EKSTENSI 2024
+        // ===============================
+        Kelas::updateOrCreate(
+            [
+                'nama_kelas'  => 'EKSTENSI-WKB',
+                'prodi_id'    => $prodi->id,
+                'program_id'  => $programEkstensi->id,
+                'angkatan_id' => $angkatan2024->id,
+            ],
+            [
+                'kapasitas' => 40,
+            ]
+        );
+
+        // ===============================
+        // REGULER 2025
+        // ===============================
+        foreach (['A', 'B', 'C', 'D', 'E'] as $kelas) {
+            Kelas::updateOrCreate(
+                [
+                    'nama_kelas'  => $kelas,
+                    'prodi_id'    => $prodi->id,
+                    'program_id'  => $programReguler->id,
+                    'angkatan_id' => $angkatan2025->id,
                 ],
                 [
                     'kapasitas' => 40,
