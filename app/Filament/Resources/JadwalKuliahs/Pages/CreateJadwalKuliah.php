3792c06e32f6bd4219a2b7f006de1c192988ec4a
nanu <ranusate19@gmail.com>
2026-09-06 20:04:34 +0800

    Update CreateJadwalKuliah.php

--- app/Filament/Resources/JadwalKuliahs/Pages/CreateJadwalKuliah.php
+++ app/Filament/Resources/JadwalKuliahs/Pages/CreateJadwalKuliah.php
@@ -4,8 +4,19 @@ namespace App\Filament\Resources\JadwalKuliahs\Pages;
 
 use App\Filament\Resources\JadwalKuliahs\JadwalKuliahResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Arr;
 
 class CreateJadwalKuliah extends CreateRecord
 {
     protected static string $resource = JadwalKuliahResource::class;
+    protected function preserveFormDataWhenCreatingAnother(array $data): array
+    {
+        return Arr::only($data, [
+            'tahun_akademik_id',
+            'kurikulum_id',
+            'hari',
+            'jam_mulai',
+            'jam_selesai',
+        ]);
+    }
 }
