6ec6af4bcb7c0b586f9114bf157080ca005810ae
nanu <ranusate19@gmail.com>
2026-09-08 00:11:10 +0800

    Update JenisAdjustment.php

--- app/Enums/Keuangan/JenisAdjustment.php
+++ app/Enums/Keuangan/JenisAdjustment.php
@@ -12,6 +12,7 @@ enum JenisAdjustment: string implements HasColor, HasLabel
     case KOREKSI_BEASISWA = 'KOREKSI_BEASISWA';
     case KOREKSI_INPUT = 'KOREKSI_INPUT';
     case PEMBATALAN_DISKON = 'PEMBATALAN_DISKON';
+    case KOREKSI_NOMINAL_SINKRONISASI = 'KOREKSI_NOMINAL_SINKRONISASI';
     case LAINNYA = 'LAINNYA';
 
     public function getLabel(): ?string
@@ -20,6 +21,7 @@ enum JenisAdjustment: string implements HasColor, HasLabel
             self::KOREKSI_BEASISWA => 'Koreksi Beasiswa (Retroaktif)',
             self::KOREKSI_INPUT => 'Koreksi Kesalahan Input',
             self::PEMBATALAN_DISKON => 'Pembatalan Diskon/Beasiswa',
+            self::KOREKSI_NOMINAL_SINKRONISASI => 'Koreksi Nominal Sinkronisasi',
             self::LAINNYA => 'Penyesuaian Lainnya',
         };
     }
@@ -30,6 +32,7 @@ enum JenisAdjustment: string implements HasColor, HasLabel
             self::KOREKSI_BEASISWA => 'info',
             self::KOREKSI_INPUT => 'warning',
             self::PEMBATALAN_DISKON => 'danger',
+            self::KOREKSI_NOMINAL_SINKRONISASI => 'success',
             self::LAINNYA => 'gray',
         };
     }
