b320b5d118de2385910323cc9ae08473ef13b3bd
nanu <ranusate19@gmail.com>
2026-09-15 11:33:55 +0800

    Create 2026_09_15_113329_make_prodi_id_nullable_on_jadwal_generator_batches_table.php

---
+++ database/migrations/2026_09_15_113329_make_prodi_id_nullable_on_jadwal_generator_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('jadwal_generator_batches', function (Blueprint $table) {
+            $table->foreignId('prodi_id')
+                ->nullable()
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jadwal_generator_batches', function (Blueprint $table) {
+            //
+        });
+    }
+};
