030caaffcadeca6cc560b61a001ffeb53a51bca9
nanu <ranusate19@gmail.com>
2026-09-20 00:46:21 +0800

    update

--- app/Filament/Resources/KurikulumMataKuliahs/KurikulumMataKuliahResource.php
+++ app/Filament/Resources/KurikulumMataKuliahs/KurikulumMataKuliahResource.php
@@ -25,8 +25,8 @@ class KurikulumMataKuliahResource extends Resource
     protected static ?string $slug = 'master-akademik/kurikulum-mata-kuliah';
     protected static ?string $modelLabel = 'Pemetaan MK Kurikulum';
     protected static ?string $pluralModelLabel = 'Pemetaan Mata Kuliah';
-    protected static ?int $navigationSort = 5;
-    
+    protected static ?int $navigationSort = 2;
+
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedMap;
     public static function form(Schema $schema): Schema
     {
